ca71532e7be477e3be28515b933af453ade4e902
DamilolaAbiona <DamilolaAbiona@users.noreply.github.com>
2022-11-27 00:32:30 +0100

    vowelCounter

---
+++ script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector(".btn"), 
+word = document.querySelector(".input-text"),
+result = document.querySelector(".result");
+btn.addEventListener("click", countVowel)
+function countVowel(){
+    let vowelCount = 0;
+    let wordVal = word.value.toLowerCase();
+    for (let i = 0; i < wordVal.length; i++){
+    let letter = wordVal[i];
+    if (letter.match(/([a,e,i,o,u])/)) {
+        vowelCount ++;
+    }
+}
+
+result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels`
+}
